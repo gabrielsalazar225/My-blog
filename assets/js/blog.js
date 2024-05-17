e51fd1d7494f59b66d925beda7f3457cf00fcd8d
gabrielsalazar225 <165944712+gabrielsalazar225@users.noreply.github.com>
2024-05-16 21:48:44 -0400

    delete updated

---
+++ assets/js/blog.js
@@ -0,0 +1,39 @@
+const themeswitcher = document.querySelector('#theme-switcher')
+let container = document.querySelector('.container');
+let mode = 'light'
+
+
+function displayBlogPosts () {
+    const blogPost = document.getElementById('blogPost');
+
+    const blogList = JSON.parse(localStorage.getItem('blogList'));
+
+    blogList.forEach(element => {
+        const child = document.createElement('div')
+        const title = document.createElement('h2')
+        const userName = document.createElement('h3')
+        const content = document.createElement('p')
+
+        title.innerHTML = element.title
+        title.innerHTML = element.userName
+        title.innerHTML = element.content
+
+        child.appendChild(title)
+        child.appendChild(userName)
+        child.appendChild(content)
+
+        blogPost.appendChild(child)
+    }); 
+
+    }
+    displayBlogPosts();
+
+    themeswitcher.addEventListener('click', function() {
+    if (mode === 'light') {
+        mode = 'dark';
+    container.setAttribute('class', 'light');    
+    } else {
+        mode = 'light';
+    container.setAttribute('class', 'dark');
+    } 
+    });
--- assets/js/styles.js
+++ assets/js/styles.js
@@ -1,34 +1,16 @@
-const username = document.getElementById('usernameEl')
-const title = document.getElementById ('titleEl')
-const comments = document.getElementById('CommentsEl')
-const submmitevent = document.getElementById('submit')
-const modeToggle = document.getElementById('mode-toggle');
-const container = document.querySelector('.container');
+// const switcherDarkLight = document.querySelector('#mode-toggle')
+// const container = document.querySelector('container-light')
+// let mode = 'light'
 
 
-// I am creating the string of the data that i will save after the event
-function saveLastBlog () {
-    const blogInfo = {
-        username : username.value.trim(),
-        title : title.value.trim(),
-        comments : comments.value.trim ()
-    }
+// switcherDarkLigth.addEventListener ('click', function()
+// {
+//     if (mode === 'light') {
+//         mode = 'dark';
+//      container.classList.add('dark');
+//      container.classList.remove('light')
 
-// I am converting the variable on a string to make possible to storage 
-    localStorage.setitem ('bloginfo', JSON.stringify(tempblogInfo))
-}
-
-submmit.addEventListener('click', function (event){
-    event.preventDefault();
-})
-
-function localStorage () {
-    const storedData= JSON.parse(localStorage.getItem('bloginfo'));
-    tempblogInfo = storedData
-}
-
-modeToggle.addEventListener('click', () => {
-    container.classList.toggle('dark-mode');
-});
-
-
+//     } else { mode = 'light'; 
+//         container.classList.add('light');
+//         container.classList.remove('dark'); }
+// });
